3e2f3d4d8be89eb07022140c583f418df4e250d1
mhd <mahdileboss2@gmail.com>
2026-01-21 19:07:02 +0100

    rollback

--- services/cardService.js
+++ services/cardService.js
@@ -1,19 +1,44 @@
-import * as cardRepo from "../Repositories/cardRepo.js";
 
-export const getCards = async (filters) => {
-  return cardRepo.findAll(filters);
-};
-
-export const getCardById = async (id) => {
-  const card = await cardRepo.findById(id);
-  if (!card) {
-    const error = new Error("Card not found");
-    error.status = 404;
-    throw error;
+import Module from "../models/Card.js"
+export async function getCards(req,res){
+try {
+    const modules = await Module.find();
+    res.status(200).json(modules);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
-  return card;
-};
+}
 
-export const createCard = async (data) => {
-  return cardRepo.create(data);
-};
+export async function getCard(req,res){
+    try {
+            const module = await Module.findById(req.params.id);
+            if (!module) {
+                return res.status(404).json({ message: 'Module not found' });
+            }
+            res.status(200).json(module);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+    }
+
+
+export async function getByCetag(req,res){
+    try {
+        const categ = req.params.category;
+            const categories = await Module.find({
+                category:categ
+            });
+            res.status(200).json(categories);
+        } catch (error) {
+            res.status(500).json({ message: error.message });
+        }
+}
+export async function createCard(req,res){
+try {
+        const newModule = new Module(req.body);
+        const savedModule = await newModule.save();
+        res.status(201).json(savedModule);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+}
